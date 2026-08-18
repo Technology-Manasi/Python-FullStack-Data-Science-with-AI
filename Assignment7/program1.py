7bd69dad6f6cd5915ebc9dff4e3a2fc653e88ddd
Manasi Rawool <manasirawool0303@gmail.com>
2026-08-18 21:17:42 +0530

    Add files via upload

---
+++ Assignment7/program1.py
@@ -0,0 +1,23 @@
+n = 5
+
+# Upper part
+for i in range(n):
+    for j in range(n - i - 1):
+        print(" ", end=" ")
+    print("*", end=" ")
+    if i != 0:
+        for j in range(2 * i - 1):
+            print(" ", end=" ")
+        print("*", end="")
+    print()
+
+# Lower part
+for i in range(n - 2, -1, -1):
+    for j in range(n - i - 1):
+        print(" ", end=" ")
+    print("*", end=" ")
+    if i != 0:
+        for j in range(2 * i - 1):
+            print(" ", end=" ")
+        print("*", end="")
+    print()
